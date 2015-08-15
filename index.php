de832933a8b9b94eda379ffbd39d19a7726feb79
DorianVasco <mail@dorianvasco.de>
2015-08-15 08:34:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php require_once 'gga-BaconIpsumGenerator.php';
 $bacon_ipsum_generator = new BaconIpsumGenerator();
-$meaty_filler = $bacon_ipsum_generator->Make_Some_Meaty_Filler('meat-and-filler', 3, true);
+print $bacon_ipsum_generator->Make_Some_Meaty_Filler('meat-and-filler', 3, true);
 ?>
